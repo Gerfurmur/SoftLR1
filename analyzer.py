bee5bad971f0267db7c02b4425e65b211b9e679a
Gerfurmur <61269368+Gerfurmur@users.noreply.github.com>
2020-09-28 19:26:38 +0300

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -6,8 +6,6 @@ def check(string):
         return False
     res = re.match(r"([a-zA-Z]+)( +-[a-zA-Z]+)*( +[a-zA-Z]+)* *\n?$", string)
     if res != None:
-        if string == res.group(0):
-            com_name = re.match(r"[a-zA-Z]+", string).group(0)
-            return True, com_name
+        return True, res.group(1)
     return False, None
 
